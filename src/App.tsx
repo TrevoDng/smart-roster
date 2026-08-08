cce84cc932aad9862bc0b04bd04cf73f9507b513
trevor <trevor279@live.com>
2026-08-08 06:56:42 +0200

    Adding manual employee company number

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename={"smart-roster"}>
+    <Router basename="/smart-roster">
       <AuthProvider>
         <div className="App">
           <Routes>
